d547f3f8b84acd6ace87fa81842fca4d679075c8
chayaleA <150287616+chayaleA@users.noreply.github.com>
2024-04-06 23:26:49 +0300

    Add files via upload

---
+++ src/service.js
@@ -0,0 +1,51 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = "http://localhost:5036";
+
+axios.interceptors.response.use(
+  response => response,
+  error => {
+    console.error('An error occurred:', error);
+    return Promise.reject(error);
+  }
+);
+
+const service = {
+  getTasks: async () => {
+    const result = await axios.get("/items");
+    return result.data;
+  },
+
+  addTask: async (name) => {
+    console.log('addTask', name);
+    const result = await axios.post(`/items`, {
+      Name: name,
+      IsComplete: false
+    });
+    return result.data;
+  },
+
+  setCompleted: async (id, isComplete) => {
+    console.log('setCompleted', { id, isComplete });
+    const answer = await axios.get(`/items`);
+    const item = answer.data.find(item => item.id === parseInt(id));
+    if (!item) {
+      console.error(`Item with id ${id} not found.`);
+      return null;
+    }
+    const result = await axios.put(`/items/${id}`, {
+      Id: item.id,
+      Name: item.name,
+      IsComplete: isComplete     
+    });
+    return result.data;
+  },
+
+  deleteTask: async (id) => {
+    console.log('deleteTask');
+    const result = await axios.delete(`/items/${id}`);
+    return result;
+  }
+};
+
+export default service; // Export the 'service' object as the default export
